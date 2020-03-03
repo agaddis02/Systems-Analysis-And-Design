3d86ae800fe60684f98508e56f1adfda147c6df3
Adam Gaddis <agaddis02@live.com>
2020-03-03 11:31:49 -0600

    Analog reader and ultrasonic sensor reader

--- Projects/Analog_Read/Analog_Read.ino
+++ Projects/Analog_Read/Analog_Read.ino
@@ -7,7 +7,7 @@ void setup() {
 void loop() {
 //  for (int fadeV = 0; fadeV <= 255; fadeV += 5)
 //  {
-//    Serial.print("Fade Value = ");
+//   Serial.print("Fade Value = ");
 //    Serial.println(fadeV);
 //    analogWrite(0,fadeV);
 //    delay(50);
@@ -21,19 +21,19 @@ void loop() {
 //    delay(50);
 //  }
 
-//  int potValue = analogRead(A0);
-//  int brightness = map(potValue, 0, 1023, 0, 255);
-//  analogWrite(9, brightness);
-//  Serial.print("Pot Value (A0) = ");
-//  Serial.println(potValue);
-//  Serial.print("Brightness Value (pin 9) = ");
-//  Serial.println(brightness);
-//  delay(500);
-
-  int sensorV = analogRead(A0);
-
-  Serial.println("Sensor Reading: ");
-  Serial.println(sensorV);
-  delay(50);
+  int potValue = analogRead(A0);
+  int brightness = map(potValue, 0, 1023, 0, 255);
+  analogWrite(9, brightness);
+  Serial.print("Pot Value (A0) = ");
+  Serial.println(potValue);
+  Serial.print("Brightness Value (pin 9) = ");
+  Serial.println(brightness);
+  delay(500);
+//
+//  int sensorV = analogRead(A0);
+//
+//  Serial.println("Sensor Reading: ");
+//  Serial.println(sensorV);
+//  delay(50);
 
 }
